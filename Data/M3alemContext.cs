f12e0ba186fe3efdd1a6ee2f40221fc171b5986a
AshManalSalim <108204094+AshManalSalim@users.noreply.github.com>
2025-05-06 18:37:09 +0100

    Update M3alemContext.cs

--- Data/M3alemContext.cs
+++ Data/M3alemContext.cs
@@ -1,1 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using M3alemAPI.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) {}
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Job> Jobs { get; set; }
+    public DbSet<Rating> Ratings { get; set; }
+}
 
